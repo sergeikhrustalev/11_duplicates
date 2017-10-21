3bb22b4fe8188fa2a6b69fcfd85b2aac0baac348
sergei <sergei@Mac-mini-sergei.local>
2017-10-21 22:00:49 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Script duplicates.py recursively searches duplicate files in specified directory
+
+# Requirements
+
+Script require python3.5+
+
+# How to use
+```bash
+$ python duplicates.py testdir
+.DS_Store - 6148 bites found on the following path:
+	testdir/.DS_Store
+	testdir/d1/.DS_Store
+	testdir/d3/.DS_Store
+	testdir/d2/.DS_Store
+1.txt - 0 bites found on the following path:
+	testdir/1.txt
+	testdir/d2/1.txt
+2.txt - 27 bites found on the following path:
+	testdir/2.txt
+	testdir/d3/2.txt
+	testdir/d2/2.txt
+pycharm-community-2017.2.3.dmg - 193274572 bites found on the following path:
+	testdir/d2/pycharm-community-2017.2.3.dmg
+	testdir/d2/distrib/pycharm-community-2017.2.3.dmg
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,60 @@
+import os
+import sys
+
+
+def get_walked_data(searchpath):
+    filelist = []
+    for dirpath, dirnames, filenames in os.walk(searchpath):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if os.path.isfile(filepath):
+
+                filelist.append(
+                    (filepath, (filename, os.path.getsize(filepath)))
+                )
+
+    return filelist
+
+
+def find_duplicate_files(filelist):
+    fileinfo_list = [fileinfo[1] for fileinfo in filelist]
+
+    duplicates_only = [
+        fileinfo for fileinfo in fileinfo_list
+        if fileinfo_list.count(fileinfo) > 1
+    ]
+
+    found_duplicates = []
+    for filepath, fileinfo in filelist:
+        if fileinfo in duplicates_only:
+            found_duplicates.append((filepath, fileinfo[0], fileinfo[1]))
+    return found_duplicates
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        sys.exit('Syntax: duplicates.py <directory_path>')
+
+    filelist = get_walked_data(sys.argv[1])
+    found_duplicates = find_duplicate_files(filelist)
+    if not found_duplicates:
+        sys.exit('Path not exist or directory is empty')
+
+    found_duplicates.sort(key=lambda file_item: file_item[1])
+    current_filename = found_duplicates[0][1]
+    current_filesize = found_duplicates[0][2]
+
+    print('{} - {} bites found on the following path: '.format(
+        current_filename, current_filesize)
+    )
+
+    for filepath, filename, filesize in found_duplicates:
+        if filename != current_filename:
+            current_filename = filename
+            current_filesize = filesize
+
+            print('{} - {} bites found on the following path: '.format(
+                current_filename, current_filesize)
+            )
+
+        print('\t{}'.format(filepath))
