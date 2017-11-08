54f423c1db00d323916f19e279544c61e541671d
sergei <sergei@Mac-mini-sergei.local>
2017-11-08 10:21:33 +0300

    fix recommended changes

--- duplicates.py
+++ duplicates.py
@@ -1,60 +1,42 @@
 import os
 import sys
+from collections import defaultdict
 
 
-def get_walked_data(searchpath):
-    filelist = []
+def get_fileinfo(searchpath):
+    fileinfo = defaultdict(list)
     for dirpath, dirnames, filenames in os.walk(searchpath):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
             if os.path.isfile(filepath):
-
-                filelist.append(
-                    (filepath, (filename, os.path.getsize(filepath)))
-                )
-
-    return filelist
+                fileinfo[filename, os.path.getsize(filepath)].append(filepath)
+    return fileinfo
 
 
-def find_duplicate_files(filelist):
-    fileinfo_list = [fileinfo[1] for fileinfo in filelist]
+def get_duplicates(fileinfo):
 
-    duplicates_only = [
-        fileinfo for fileinfo in fileinfo_list
-        if fileinfo_list.count(fileinfo) > 1
-    ]
-
-    found_duplicates = []
-    for filepath, fileinfo in filelist:
-        if fileinfo in duplicates_only:
-            found_duplicates.append((filepath, fileinfo[0], fileinfo[1]))
-    return found_duplicates
+    return {
+        namesize: filelist
+        for namesize, filelist in fileinfo.items()
+        if len(filelist) > 1
+    }
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         sys.exit('Syntax: duplicates.py <directory_path>')
 
-    filelist = get_walked_data(sys.argv[1])
-    found_duplicates = find_duplicate_files(filelist)
-    if not found_duplicates:
+    fileinfo = get_fileinfo(sys.argv[1])
+    if not fileinfo:
         sys.exit('Path not exist or directory is empty')
 
-    found_duplicates.sort(key=lambda file_item: file_item[1])
-    current_filename = found_duplicates[0][1]
-    current_filesize = found_duplicates[0][2]
+    duplicates = get_duplicates(fileinfo)
+    for namesize, filelist in duplicates.items():
 
-    print('{} - {} bites found on the following path: '.format(
-        current_filename, current_filesize)
-    )
+        print('{} - {} bites found on the following path: '.format(
+            namesize[0],
+            namesize[1]
+        ))
 
-    for filepath, filename, filesize in found_duplicates:
-        if filename != current_filename:
-            current_filename = filename
-            current_filesize = filesize
-
-            print('{} - {} bites found on the following path: '.format(
-                current_filename, current_filesize)
-            )
-
-        print('\t{}'.format(filepath))
+        for filepath in filelist:
+            print('\t{}'.format(filepath))
